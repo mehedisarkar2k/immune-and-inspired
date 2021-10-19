628ef75a7944f0a1caeb62c02606bf3d87ca2f44
Mehedi Hasan Sarkar <mehedisarkar@hotmail.com>
2021-10-19 18:18:03 +0600

    added env file

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCOkJZi3pPypJySmrAhWg4Oz4mkjOJ26uo",
-  authDomain: "immune-and-inspired.firebaseapp.com",
-  projectId: "immune-and-inspired",
-  storageBucket: "immune-and-inspired.appspot.com",
-  messagingSenderId: "719499566174",
-  appId: "1:719499566174:web:91f3d976a14afd8c5ba8cf",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export default firebaseConfig;
